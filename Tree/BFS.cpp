964e6d25b709307b835d6e949e3ceb3f2877e00b
PrasadDhumal-P24 <suvarnadhumal121@gmail.com>
2026-08-05 23:36:18 -0700

    write clean code of BFS

--- Tree/BFS.cpp
+++ Tree/BFS.cpp
@@ -18,39 +18,7 @@ public:
     }
 };
 
-void preorder(node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    cout << root->data << " ";
-    preorder(root->left);
-    preorder(root->right);
-}
-void inorder(node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    inorder(root->left);
-    cout << root->data << " ";
-    inorder(root->right);
-}
-void postorder(node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-
-    postorder(root->left);
-    postorder(root->right);
-    cout << root->data << " ";
-}
-
-void leveorder(node *root)
+void levelorder(node *root)
 {
     if (root == NULL)
     {
@@ -67,10 +35,13 @@ void leveorder(node *root)
         cout << current->data << " ";
 
         if (current->left != NULL)
+        {
             q.push(current->left);
-
+        }
         if (current->right != NULL)
+        {
             q.push(current->right);
+        }
     }
 }
 int main()
@@ -86,19 +57,7 @@ int main()
     root->right->left = new node(60);
     root->right->right = new node(70);
 
-    cout << " preorder : ";
-    preorder(root);
-    cout << endl;
-
-    cout << " inorder : ";
-    inorder(root);
-    cout << endl;
-
-    cout << " postorder : ";
-    postorder(root);
-    cout << endl;
-
-    leveorder(root);
+    levelorder(root);
 
     return 0;
 }
