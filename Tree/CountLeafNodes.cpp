31813db6ce9094a6c9745aecfead454e251b6690
PrasadDhumal-P24 <suvarnadhumal121@gmail.com>
2026-08-10 23:23:44 -0700

    count LeafNodes in tree

---
+++ Tree/CountLeafNodes.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int value)
+    {
+
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int CountLeafNodes(node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    if (root->left == NULL && root->right == NULL)
+    {
+
+        return 1;
+    }
+
+    return CountLeafNodes(root->left) + CountLeafNodes(root->right);
+}
+
+int main()
+{
+
+    node *root = new node(10);
+
+    root->left = new node(20);
+    root->right = new node(30);
+    root->left->left = new node(40);
+    root->left->right = new node(50);
+    root->right->left = new node(60);
+    root->right->right = new node(70);
+
+    cout << CountLeafNodes(root);
+}
